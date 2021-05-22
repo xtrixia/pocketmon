5b7b9075ed05291653b3156b67f2023e97997d5b
Annisa Feryannie <aferyannie@gmail.com>
2021-05-22 21:46:12 +0700

    styles: add margin top stylings

--- src/root/styles.js
+++ src/root/styles.js
@@ -18,6 +18,14 @@ export const marginRightMd = css`
   margin-right: ${SPACINGS.md};
 `;
 
+export const marginTopMd = css`
+  margin-top: ${SPACINGS.md};
+`;
+
+export const marginTopXxl = css`
+  margin-top: ${SPACINGS.xxl};
+`;
+
 export const marginBottomMd = css`
   margin-bottom: ${SPACINGS.md};
 `;

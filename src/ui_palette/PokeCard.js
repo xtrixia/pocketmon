64a79d61eb6f49ad470bfa9506d55457448000cc
Annisa Feryannie <aferyannie@gmail.com>
2021-05-22 11:38:41 +0700

    refactor: adjust link href on pokecard

--- src/ui_palette/PokeCard.js
+++ src/ui_palette/PokeCard.js
@@ -3,38 +3,31 @@ import { css } from "@emotion/css";
 import clsx from "clsx";
 
 import Typography from "./Typography";
+import PokeImg from "./PokeImg";
 
 import { COLORS } from "../root/colors";
 import { SPACINGS } from "../root/spacings";
 import { BREAKPOINTS } from "../root/breakpoints";
 
-function PokeCard({ className, imgUrl, onClick, title, totalOwned }) {
+function PokeCard({ className, imgUrl, title, totalOwned }) {
   return (
     <Link
-      to={`/profile/${title}`}
-      onClick={onClick}
+      to={`/profile/${title}?img=${imgUrl}`}
       className={clsx(
         className,
         css`
           filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
           display: flex;
-          // box-shadow: 1px 1px 5px;
           border-radius: 5px;
           text-decoration: none;
         `
       )}
     >
-      <img
-        src={imgUrl}
+      <PokeImg
+        img={imgUrl}
         alt={title}
-        className={css`
-          width: 100px;
-          background: ${COLORS.white2};
-
-          @media (min-width: ${BREAKPOINTS.sm}) {
-            width: 250px;
-          }
-        `}
+        type="small"
+        background={COLORS.white2}
       />
       <div
         className={css`
@@ -44,21 +37,12 @@ function PokeCard({ className, imgUrl, onClick, title, totalOwned }) {
           display: flex;
           justify-content: center;
           flex-direction: column;
-
           @media (min-width: ${BREAKPOINTS.sm}) {
             padding: ${SPACINGS.lg};
           }
         `}
       >
-        <Typography
-          variant="h4"
-          className={css`
-            font-family: "Lora", serif;
-            color: ${COLORS.dark};
-          `}
-        >
-          {title}
-        </Typography>
+        <Typography variant="subheading2">{title}</Typography>
         <Typography
           variant="body"
           className={css`

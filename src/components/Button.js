f280408e479b44ae595114efb51613a6295565fd
Annisa Feryannie <aferyannie@gmail.com>
2021-05-23 17:25:20 +0700

    styles: move styling inline

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,28 +1,31 @@
 import { css } from "@emotion/css";
+import clsx from "clsx";
 
 import { SPACINGS } from "../root/spacings";
 import { COLORS } from "../root/colors";
 
-function Button({ children, fullWidth, type, ...props }) {
-  let borderStyle = `1px solid ${COLORS.dark}`;
-  let backgroundStyle = COLORS.white;
-  let boxShadowStyle = "1px 2px";
-  if (type === "outline") {
-    borderStyle = "none";
-    backgroundStyle = "none";
-    boxShadowStyle = 0;
-  }
-
+function Button({ children, className, fullWidth, type, ...props }) {
   return (
     <button
-      className={css`
-        width: ${fullWidth ? "100%" : "fit-content"};
-        padding: ${SPACINGS.xs};
-        background: ${backgroundStyle};
-        box-shadow: ${boxShadowStyle};
-        border-radius: 5px;
-        border: ${borderStyle};
-      `}
+      className={clsx(
+        className,
+        css`
+          width: ${fullWidth ? "100%" : "fit-content"};
+          padding: ${SPACINGS.xs};
+          background: ${type === "outline" ? "none" : COLORS.white};
+          box-shadow: ${type === "outline" ? 0 : "2px 2px"};
+          border-radius: 5px;
+          border: ${type === "outline" ? "none" : `1px solid ${COLORS.dark}`};
+          :hover {
+            background: ${COLORS.white2};
+            transition: ease 0.5s;
+          }
+          :active {
+            box-shadow: 1px 2px;
+            transform: translateY(3px);
+          }
+        `
+      )}
       {...props}
     >
       {children}
